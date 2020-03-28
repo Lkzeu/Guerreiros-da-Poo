97d185ba24ef4e0ff85bd044d04e3df2195ffd21
Lucas Alves <52470996+Lkzeu@users.noreply.github.com>
2020-03-27 21:05:33 -0300

    projeto finalizado

--- src/lutadores/Luta.java
+++ src/lutadores/Luta.java
@@ -5,58 +5,50 @@ import java.util.Random;
 public class Luta {
 
    public void lutar(Guerreiro lutador, Guerreiro oponente) {
-      
+
       if (lutador.getCodigoLutador() != oponente.getCodigoLutador()) {
-         
+
          if (lutador.getEnergia() == 0 | oponente.getEnergia() == 0) {
-            
+
             System.out.println("não é possível lutar. Vá comer e depois volte!");
-         
+
          } else {
+            String lutador1 = null;
+            String lutador2 = null;
+
             while(true) {
-               int ordemDeAtaque = new Random(System.nanoTime()).nextInt(2); 
-               
+               int ordemDeAtaque = new Random(System.nanoTime()).nextInt(2);
+
                if (ordemDeAtaque == 0) {
-                   if (lutando(lutador, oponente) == "Venceu") {
-                      System.out.format("%s GANHOU!!!%n", lutador.getNome().toUpperCase());
-                      
-                      lutador.setVitorias(lutador.getVitorias() + 1);
-                      oponente.setDerrotas(oponente.getDerrotas() + 1);
-                      break;
-                   }
-                   if (lutando(oponente, lutador) == "Venceu") {
-                      System.out.format("%s GANHOU!!!%n", oponente.getNome().toUpperCase());
-                      
-                      oponente.setVitorias(oponente.getVitorias() + 1);
-                      lutador.setDerrotas(lutador.getDerrotas() + 1);
-                      break;
-                   }
-              
+                   lutador1 = lutando(lutador, oponente);
+                   if (lutador1 == "Venceu") { break; }
+
+                   lutador2 = lutando(oponente, lutador);
+                   if (lutador2 == "Venceu") { break; }
+
                } else {
-                  
-                  if (lutando(oponente, lutador) == "Venceu") {
-                     System.out.format("%s GANHOU!!!%n", oponente.getNome().toUpperCase());
-                     
-                     oponente.setVitorias(oponente.getVitorias() + 1);
-                     lutador.setDerrotas(lutador.getDerrotas() + 1);
-                     break;
-                  }
-                  if (lutando(lutador, oponente) == "Venceu") {
-                     System.out.format("%s GANHOU!!!%n", lutador.getNome().toUpperCase());
-                     
-                     lutador.setVitorias(lutador.getVitorias() + 1);
-                     oponente.setDerrotas(oponente.getDerrotas() + 1);
-                     break;
-                  }
+                  lutador2 = lutando(oponente, lutador);
+                  if (lutador2 == "Venceu") { break; }
+
+                  lutador1 = lutando(lutador, oponente);
+                  if (lutador1 == "Venceu") { break; }
                }
-            } // while end
-         } // if energia end
-         
+            }
+            if (lutador1 == "Venceu") { lutaFinalizada(lutador, oponente); }
+            if (lutador2 == "Venceu") { lutaFinalizada(oponente, lutador); }
+         }
       } else {
          System.out.println("NÃO É POSSÍVEL LUTAR CONSIGO MESMO!");
       }
    }
-   
+
+   public void lutaFinalizada(Guerreiro vencedor, Guerreiro perdedor) {
+      System.out.format("%s GANHOU!!!%n", vencedor.getNome().toUpperCase());
+
+      vencedor.setVitorias(vencedor.getVitorias() + 1);
+      perdedor.setDerrotas(perdedor.getDerrotas() + 1);
+   }
+
    public String lutando(Guerreiro lutador1, Guerreiro lutador2) {
       if (lutador1.getEnergia() > 0) {
          if (lutador1.atacar()) {
@@ -66,7 +58,7 @@ public class Luta {
             }
          }
       }
-      
+
       return "";
    }
 }
